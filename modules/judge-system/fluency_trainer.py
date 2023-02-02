39820fcbe051cc350d14e1fc708a7d142ba207d3
BugraHamza <bugrahamza@gmail.com>
2023-02-03 01:33:30 +0300

    bugfix fluency trainer if_exists

--- modules/judge-system/fluency_trainer.py
+++ modules/judge-system/fluency_trainer.py
@@ -99,7 +99,7 @@ def objective(trial):
 
 
 if __name__ == '__main__':
-    study = optuna.create_study(study_name=f'fluency_study_{time.time()}', storage='sqlite:///fluency_study.db',
+    study = optuna.create_study(study_name=f'fluency_study', storage='sqlite:///fluency_study.db',
                                 direction="minimize", load_if_exists=True,
                                 pruner=optuna.pruners.SuccessiveHalvingPruner())
     study.optimize(objective, n_trials=50, gc_after_trial=True)
